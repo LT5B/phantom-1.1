3f23b47ef1d18e83e5e3dc095dd3a7ad953b3b0a
LT5B <engeleditorfpe@gmail.com>
2026-08-31 20:24:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
 
-echo "Downloading txz file..."
-curl -s -L -o phantom-1.1.txz https://raw.githubusercontent.com/LT5B/phantom-1.1/refs/heads/main/phantom-1.1.txz
-echo "Decompressing file..."
-tar xJf phantom-1.1.txz
-echo "Running installer..."
-cd phantom-1.1
-sudo ./Configure
-echo "Cleaning..."
-cd ..
-rm phantom-1.1.txz
-rm -rf phantom-1.1
