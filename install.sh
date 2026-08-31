ea1fdbb4d345c9b06c33151aecf9bad6a7e07529
LT5B <engeleditorfpe@gmail.com>
2026-08-31 20:34:40 +0700

    Update and rename installer.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Downloading txz file..."
+curl -s -L -o "https://raw.githubusercontent.com/LT5B/phantom-1.1/refs/heads/main/phantom-1.1.txz" "$HOME/phantom-1.1.tar.gz"
+echo "Decompressing file..."
+tar zxf $HOME/phantom-1.1.tar.gz
+echo "Running installer..."
+cd $HOME/phantom
+sudo ./Configure
+echo "Cleaning..."
+rm $HOME/phantom-1.1.tar.gz
+rm -rf $HOME/phantom
